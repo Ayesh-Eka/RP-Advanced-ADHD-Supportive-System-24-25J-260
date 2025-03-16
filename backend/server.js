ef2b7ba738d84d512e0784a0d5d7e4f38b2f2a51
RanushSanjula <ranush1999san@gmail.com>
2025-03-16 18:48:56 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -48,36 +48,4 @@ app.listen(PORT, async () => {
 
 
 
-//Ranush
-app.post("/predictPriority", (req, res) => {
-  const { category, days_to_deadline, interest_level, duration, age, gender } = req.body;
-
-  const pythonProcess = spawn("python", ["/TaskPrioritizeModel/model_api_task.py", category, days_to_deadline, interest_level, duration, age, gender]);
-
-  let responseSent = false;
-
-  pythonProcess.stdout.on("data", (data) => {
-      if (!responseSent) {
-          res.json({ priority: data.toString().trim() });
-          responseSent = true;
-      }
-  });
-
-  pythonProcess.stderr.on("data", (data) => {
-      console.error(`Error: ${data}`);
-      if (!responseSent) {
-          res.status(500).json({ error: "Internal Server Error" });
-          responseSent = true;
-      }
-  });
-
-  pythonProcess.on("close", (code) => {
-      if (!responseSent) {
-          // Handle case where pythonProcess exits but no response was sent
-          res.status(500).json({ error: "Unexpected error occurred" });
-      }
-  });
-});
-
-
 
